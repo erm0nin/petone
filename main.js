b797812637740a46f2fd44f4f52fdb4088ce4a90
ermonindima <ermonindima0@gmail.com>
2024-05-30 14:32:29 +0300

    1.10

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 window.addEventListener('scroll', ()=>{
-  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 200);
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100);
 })
+
+// SHOW FAQs SECTION
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+  faq.addEventListener('click', ()=>{
+    faq.classList.toggle('open');
+  });
+  });
